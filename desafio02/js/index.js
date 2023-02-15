5efb0e834abc4fd21a8b70ff5f3b57a2b586419f
Cartaxo1 <cartaxojoao1@gmail.com>
2023-02-14 21:10:21 -0300

    sound commit

--- desafio02/js/index.js
+++ desafio02/js/index.js
@@ -19,8 +19,9 @@ function countdown() {
     let minutes = Number(minutesDisplay.textContent);
     updateTimerDisplay(minutes, 0);
 
-    if (minutes <= 0) {
+    if (minutes === 0 && seconds === 0) {
       updateTimerDisplay(25, 0);
+      timeEnd()
       return;
     }
 
@@ -53,3 +54,72 @@ buttonSub.addEventListener("click", function () {
     minutesDisplay.textContent = 00;
   }
 });
+
+const buttons = document.querySelectorAll(".button");
+
+for (const button of buttons) {
+  button.addEventListener("click", handleButtonClick);
+}
+
+function handleButtonClick(event) {
+  const bg = event.currentTarget.querySelector(".bg");
+  const design = event.currentTarget.querySelector(".design");
+
+  const activeBgButton = document.querySelector(".button .bg.active");
+  if (activeBgButton) {
+    activeBgButton.classList.remove("active");
+  }
+
+  const activeDesign = document.querySelector(".button .design.active-fill");
+  if (activeDesign) {
+    activeDesign.classList.remove("active-fill");
+  }
+
+  bg.classList.add("active");
+  design.classList.add("active-fill");
+}
+
+const buttonForest = document.querySelector(".button.forest");
+const buttonRain = document.querySelector(".button.rain");
+const buttonCoffe = document.querySelector(".button.coffe");
+const ButtonFire = document.querySelector(".button.fireplace");
+
+const forestAudio = new Audio("./src/Floresta.wav");
+const rainAudio = new Audio("./src/Chuva.wav");
+const coffeAudio = new Audio("./src/Cafeteria.wav");
+const fireAudio = new Audio("./src/Lareira.wav");
+const bellSound = new Audio(
+  "https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true"
+);
+
+buttonForest.addEventListener("click", soundForest);
+
+function soundForest() {
+  forestAudio.loop = true;
+  forestAudio.play();
+}
+
+buttonRain.addEventListener("click", soundRain);
+
+function soundRain() {
+  rainAudio.loop = true;
+  rainAudio.play();
+}
+
+buttonCoffe.addEventListener("click", coffeSound);
+
+function coffeSound() {
+  coffeAudio.loop = true;
+  coffeAudio.play();
+}
+
+ButtonFire.addEventListener("click", soundFireplace);
+
+function soundFireplace() {
+  fireAudio.loop = true;
+  fireAudio.play();
+}
+
+function timeEnd() {
+  bellSound.play()
+}
